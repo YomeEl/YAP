701e7ef3b72a8215e377897d2f54494180ecce5f
Yaroslav <umpepatopChannel@gmail.com>
2025-06-17 03:25:28 +0500

    last task done but not blacked yet

--- PythonLabs/27/TreeWork65.py
+++ PythonLabs/27/TreeWork65.py
@@ -3,3 +3,64 @@
 числа. После этого необходимо, привести дерево к АВЛ-сбалансированному виду, выполнив для
 LL-поворот. Известно, что требуется не более одного такого поворота.
 Вывести корень полученного дерева."""
+
+# examples 
+#   (((2)5)10) -> ((2)5(10))
+#   ((((1)2)3)4(5(6))) -> (((1)2(3))4(5(6)))
+#   ((((1)2(3))4)5(6(7))) -> (((1)2((3)4))5(6(7)))
+
+from helpers import Helpers
+from TreeWork26 import BinTreeParser
+from bin_tree import print_tree
+
+def test(name, tree_string, expect):
+    print("====")
+    print(f"INPUT\n\t{tree_string}")
+
+    parser = BinTreeParser()
+    tree = parser.parse(tree_string)
+    print_tree(tree, "\t")
+    tree.balance()
+
+    print(f"EXPECTED RESULT\n\t{expect}")
+    print(f"ACTUAL RESULT\n\t{tree}")
+    print_tree(tree, "\t")
+    print("TEST RESULT")
+    print("\tSUCCESS" if str(tree) == expect else "\tERROR")
+    print("====")
+
+
+def run_test():
+    test("No left subtree", "(((2)5)10)", "((2)5(10))")
+    test("No right subtree", "((((1)2)3)4(5(6)))", "(((1)2(3))4(5(6)))")
+    test("No right subtree in left subtree", "((((1)2(3))4)5(6(7)))", "(((1)2((3)4))5(6(7)))")
+
+
+def run_manual():
+    print("<дерево> ::= ((<левое поддерево>)<вершина>(<правое поддерево>))")
+    print("           | ((<левое поддерево>)<вершина>)")
+    print("           | (<вершина>(<правое поддерево>))")
+    print("<левое поддерево> ::= <дерево>")
+    print("<правое поддерево> ::= <дерево>")
+
+    promt = "Введите <дерево>: "
+
+    def converter(string):
+        parser = BinTreeParser()
+        return parser.parse(string)
+
+    condition = lambda _: True
+    tree = Helpers.input_with_condition(promt, "", converter, condition)
+    tree.balance()
+    print(f"Дерево после балансировки LL-поворотом: {tree}")
+
+
+if __name__ == "__main__":
+    promt = "Выберите режим работы"
+    variants = [("Ввод с клавиатуры", True), ("Запустить тесты", False)]
+    is_manual = Helpers.input_select(promt, variants)
+
+    if is_manual:
+        run_manual()
+    else:
+        run_test()
--- PythonLabs/27/bin_tree.py
+++ PythonLabs/27/bin_tree.py
@@ -3,12 +3,27 @@ class TreeNode:
         self.value = value
         self.left = None
         self.right = None
+        self.height = 1
 
     def __str__(self):
         left = "" if not self.left else str(self.left)
         right = "" if not self.right else str(self.right)
         return f"({left}{self.value}{right})"
+    
+    def update_height(self):
+        if self.left: 
+            self.left.update_height()
+            left_height = self.left.height
+        else:
+            left_height = 0
 
+        if self.right: 
+            self.right.update_height()
+            right_height = self.right.height
+        else:
+            right_height = 0
+
+        self.height = 1 + max(left_height, right_height)
 
 class BinTree:
     def __init__(self):
@@ -44,6 +59,54 @@ class BinTree:
         else:
             self.__insert_node(TreeNode(value))
 
+    def update_heights(self):
+        if self.root is None: return
+        self.root.update_height()
+
+    def balance(self):
+        self.update_heights()
+        self.__balance(self.root, None)
+
+    def __rotate_ll(self, node, parent):
+        is_root = parent is None 
+        if not is_root: is_left = parent.left == node
+
+        lr = node.left.right
+        new_root = node.left
+
+        new_root.right = node
+        new_root.right.left = lr
+
+        if is_root:
+            self.root = new_root
+        elif is_left:
+            parent.left = new_root
+        else:
+            parent.right = new_root
+
+
+    def __balance(self, node, parent):
+        if node is None: return
+        left_height = node.left.height if node.left else 0
+        right_height = node.right.height if node.right else 0
+        balance = left_height - right_height
+        if balance > 1:
+            self.__rotate_ll(node, parent)
+            return
+        
+        self.__balance(node.left, node)
+        self.__balance(node.right, node)
+
+    def __find_ll_imbalance(self, parent_node, node):
+        left_height = node.left.height if node.left else 0
+        right_height = node.right.height if node.right else 0
+        balance = left_height - right_height
+        if balance == 2:
+            return (parent_node, node)
+        left_result = self.__find_ll_imbalance(node, node.left)
+        right_result = self.__find_ll_imbalance(node, node.right)
+
+
     def __insert_node(self, node):
         parent = None
         next_parent = self.root
@@ -54,3 +117,21 @@ class BinTree:
             parent.left = node
         else:
             parent.right = node
+
+def print_tree(tree: BinTree, prefix = ""):
+    tree.update_heights()
+    root = tree.root
+    nodes = []
+    __collect(root, 0, nodes)
+    
+    lines = [[] for _ in range(root.height)]
+    for node, level in nodes:
+        for i, line in enumerate(lines):
+            line.append(str(node) if i == level else " ")
+    for line in lines:
+        print(prefix + " ".join(line)) 
+
+def __collect(node, level, acc):
+    if node.left: __collect(node.left, level + 1, acc)
+    acc.append((node.value, level))
+    if node.right: __collect(node.right, level + 1, acc)
